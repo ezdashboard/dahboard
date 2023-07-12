b231abb723252734dc74d922765eae5679eb04d9
vikas <vikas.pathak341@gmail.com>
2023-07-12 18:25:56 +0530

    update bakend url

--- .next/static/I_YRbrVWxlXIqLCE6csWb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[blogdetail]","\u002Fabout","\u002Fblog\u002F[blogdetail]","\u002Fblogs","\u002Fpricing","\u002Fservices","\u002Fusers\u002F[edituser]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
